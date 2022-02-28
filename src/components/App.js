ffa88cff7d7b32385085db36887e908fdcee0473
Vi Vo <aaronvo118@gmail.com>
2022-02-28 16:49:47 -0600

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,16 @@ import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  
+  
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? 
+      <QuestionForm/> 
+      : <QuestionList  />}
+      
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,55 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((r) => r.json())
+      .then((questions) => setQuestions(questions))
+  }, []);
+
+  function handleDeleteClick(id){
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: 'DELETE',
+    })
+    .then( response => response.json())
+    .then( () => { setQuestions( questions.filter( q => q.id !== id) ) })
+  }
+
+  function handlePatchClick(id , correctIndex){
+    console.log(id, correctIndex)
+    fetch(`http://localhost:4000/questions/${id}`,{
+      method: 'PATCH',
+      headers: {"Content-Type": "application/json"},
+      body:JSON.stringify({ correctIndex }),
+    })
+    .then( response => response.json())
+    .then( (updatedQuestion) => {
+      const updatedQuestions = questions.map( (q) => {
+        if (q.id === updatedQuestion.id) return updatedQuestion
+        return q
+      })
+      setQuestions(updatedQuestions)
+    })
+  }
+
+  const questionList = questions.map(q => (
+    <QuestionItem
+      key={q.id}
+      question={q}
+      onDeleteClick={handleDeleteClick}
+      onPatchClick={handlePatchClick}
+    />
+  ))
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionList}
+      </ul>
     </section>
   );
 }
